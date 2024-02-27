ace1c5a77ab1ad74b5839a6d52ae3833ed0177c1
LKH <105769152+CharmStrange@users.noreply.github.com>
2024-02-28 00:04:43 +0900

    Create AngryMilitia.c

---
+++ AngryMilitia.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include <stdlib.h>
+
+#define ROW 9
+#define COLUMN 9
+
+unsigned char Universe[ROW][COLUMN];
+
+unsigned char is_available() {
+    unsigned char state;
+    
+    //for(unsigned char index = 0; index)
+    
+    return state;
+}
+
+int main() {
+    if (ROW != COLUMN) {
+        exit(1);
+    }
+}
