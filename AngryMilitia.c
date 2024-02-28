d8402b60ce3a87e7eb3ca986a55bf7a37126f312
LKH <105769152+CharmStrange@users.noreply.github.com>
2024-02-28 17:34:10 +0900

    Update AngryMilitia.c

--- AngryMilitia.c
+++ AngryMilitia.c
@@ -8,12 +8,24 @@ typedef struct Farmland{
     unsigned char fertility;
     unsigned char moisture;
     unsigned char light;
+    unsigned char scale;
 }Farmland;
 
 Farmland Farm[ROW][COLUMN];
 
+void intialize_Farmland() {
+    for( unsigned char row = 0; row < ROW; row++ ) {
+        for( unsigned char column = 0; column < COLUMN; column++ ) {
+            Farm[row][column].fertility = 1;
+            Farm[row][column].moisture = 1;
+            Farm[row][column].light = 1;
+            Farm[row][column].scale = 1;
+        }
+    }
+}
+
 unsigned char is_available() {
-    unsigned char state;
+    unsigned char condition;
     
     for( unsigned char row = 0; row < ROW; row++ ) {
         for( unsigned char column = 0; column < COLUMN; column++ ) {
@@ -21,7 +33,7 @@ unsigned char is_available() {
         }
     }
     
-    return state;
+    return condition;
 }
 
 int main() {
