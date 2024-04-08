5ee4c425ba3bdfba6f18379bcc5d5d97dec7e449
LKH <105769152+CharmStrange@users.noreply.github.com>
2024-04-08 18:38:38 +0900

    Create PixelFishing.c

---
+++ PixelFishing.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Spot{
+    unsigned char status;
+    struct Spot *ptr;
+}Spot;
+
+void init(){
+    
+}
+
+void simulate(){
+    
+}
+
+int main() {
+    init();
+    simulate();
+}
