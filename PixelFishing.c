3bc198d8fc0bbf6c0e12a68fecdbbbb6a6548b40
LKH <105769152+CharmStrange@users.noreply.github.com>
2024-04-29 20:37:32 +0900

    Update PixelFishing.c

--- PixelFishing.c
+++ PixelFishing.c
@@ -1,138 +1,74 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <ctype.h>
 #include <math.h>
-#include <time.h>
-
 #define EULER_NUMBER 2.71828
-#define EULER_NUMBER_F 2.71828182846
-#define EULER_NUMBER_L 2.71828182845904523536
 
+typedef unsigned char UC;
+typedef struct Artifact{
+    UC Alpha;
+    UC Beta;
+    UC Gamma;
+    UC Delta;
+    
+    float sigmoid;
+    char signature;
+} Artifact;
+
+char *type[20] = {
+        "Epsilon", "Zeta", "Eta", "Theta", 
+        "Iota", "Kappa", "Lambda", "Mu",
+        "Nu", "Xi", "Omicron", "Pi", 
+        "Rho", "Sigma", "Tau", "Upsilon", 
+        "Phi", "Chi", "Psi", "Omega"
+};
+ 
 double sigmoid(double n) {
     return (1 / (1 + pow(EULER_NUMBER, -n)));
+} 
+ 
+void Control( void* function(), Artifact state){
+    function(state);
 }
 
-float sigmoidf(float n) {
-    return (1 / (1 + powf(EULER_NUMBER_F, -n)));
-}
-
-long double sigmoidl(long double n) {
-    return (1 / (1 + powl(EULER_NUMBER_L, -n)));
-}
-
-// for Option Control
-#define initialize_v1() do {for (unsigned char r = 0; r < 3; r++){ \
-            for (unsigned char c = 0; c < 3; c++){ \
-                Pixels[r][c] = (Spot){0, NULL};} \
-            } printf("\nSpots successfully created!\n"); \
-        } while (0)
-        
-#define initialize_v2(parameter_1) do {for (unsigned char r = 0; r < 3; r++){ \
-            for (unsigned char c = 0; c < 3; c++){ \
-                Pixels[r][c] = (Spot){0, NULL};} \
-            } printf("\nSpots successfully created!\n"); \
-            parameter_1(); \
-        } while (0)
-        
-#define initialize_v3(parameter_1, parameter_2) do {for (unsigned char r = 0; r < 3; r++){ \
-            for (unsigned char c = 0; c < 3; c++){ \
-                Pixels[r][c] = (Spot){0, NULL};} \
-            } printf("\nSpots successfully created!\n"); \
-            parameter_1(); parameter_2(); \
-        } while (0)
-
-#define medium(_v1, _v2, _v3, name, ...) name
-#define initialize(...) medium(_v1, ##__VA_ARGS__, initialize_v3, initialize_v2, initialize_v1)(__VA_ARGS__)
-
-// unsigned char [8bit]
-// self pointer [8bit]
-// = [16bit]
-typedef struct Spot {
-    unsigned char status;
-    struct Spot *ptr;
-} Spot;
-
-unsigned char fishes = 0;
-Spot Pixels[3][3];
-
-void Fishing(unsigned char r, unsigned char c);
-void *simulate(){
-    printf("\nSimulation\n");
-    Fishing(0, 0);
-}
-
-void *attribute(){
-    for (unsigned char r = 0; r < 3; r++){
-        for (unsigned char c = 0; c < 3; c++){
-            printf("/ %p ", &Pixels[r][c]);
-            printf("[%d] ", sizeof(Pixels[r][c]));
-        }
-    }
-    printf("\n");
-}
-
-void *setState(){
-    for (unsigned char r = 0; r < 3; r++){
-        for (unsigned char c = 0; c < 3; c++){
-            unsigned char status = rand()%255; 
-            Pixels[r][c].status = status;
-        }
-    }
+void* Alpha( Artifact state ){
     
-    // connecting
-    Pixels[0][0].ptr = &Pixels[0][1];
-    Pixels[0][1].ptr = &Pixels[0][2];
-    Pixels[0][2].ptr = &Pixels[1][0];
-    
-    Pixels[1][0].ptr = &Pixels[1][1];
-    Pixels[1][1].ptr = &Pixels[1][2];
-    Pixels[1][2].ptr = &Pixels[2][0];
-    
-    Pixels[2][0].ptr = &Pixels[2][1];
-    Pixels[2][1].ptr = &Pixels[2][2];
-    Pixels[2][2].ptr = &Pixels[0][0];
-}  
-
-void *printState(){
-    printf("\n");
-    for (unsigned char r = 0; r < 3; r++){
-        for (unsigned char c = 0; c < 3; c++){
-            printf("%d\n", Pixels[r][c].status);
-            printf("[this %p] -> [%p]\n\n", &Pixels[r][c], Pixels[r][c].ptr);
-        }
-    }
-    printf("\n"); 
 }
 
-void Fishing(unsigned char r, unsigned char c){
-    char parameter1 = rand()%127;
-    char parameter2 = rand()%127;
-    double probability;
-    probability = sigmoid(parameter1 - parameter2);
+void* Beta( Artifact state ){
     
-    if (probability > 0.5) {
-        Pixels[r][c].status--;
-        fishes++;
-        printf("\nYou got fish!\n");
-        printf(" ~~ ><(((º> ~~~\n");
-    }
-    else {
-        printf("\nThere was nothing... [%f]\n", probability);
+}
+
+void* Gamma( Artifact state ){
+    
+}
+
+void* Delta( Artifact state ){
+    
+}
+
+Artifact Maker( char Alpha, char Beta, char Gamma, char Delta ){
+    
+}
+
+void Begin(  ){
+    char buffer[6]; 
+    printf("Type Something : ");
+    fgets(buffer, sizeof(buffer), stdin); 
+
+    if (buffer[0] != '\0') { 
+        printf("%s", buffer); 
     }
 }
 
-int main() {
-    srand(time(NULL));
+int main(){
+    Begin();
     
-    // max 2 parameters can be entered in initialize ... none available, one available, two available, three NO!
-    // function parameters of initialize() method :
-    // attribute
-    // simulate
-    // setState
-    // printState
+    Artifact test = { 'A', 'B', 'G', 'D', 0.5, *type[15] };
     
-    initialize();
-    initialize(attribute);
-    initialize(attribute, simulate);
-    initialize(setState, printState);
+    Control( Alpha, test );
+    Control( Beta, test );
+    Control( Gamma, test );
+    Control( Delta, test );
+
     return 0;
 }
